8abf838d6f255c704f2be420eca710dab9e9c914
menachem95 <menachemf95@gmail.com>
2022-06-24 14:46:09 +0300

    with gui

--- lesson45/Exercise 3/script.js
+++ lesson45/Exercise 3/script.js
@@ -5,7 +5,7 @@ const showPower = document.querySelector("#showPower");
 
 
 const gui = document.querySelector("#gui");
-
+gui.style.width = "0%"
 
 
 
@@ -16,15 +16,40 @@ function checkPassword() {
     const upper = /[A-Z]/.test(str);
     const lower = /[a-z]/.test(str);
     const other = /[^\w]/.test(str);
-    if (pass_length >= 5 && upper && lower && other) {
+    const number = /[0-9]/.test(str);
+
+    if (pass_length === 0){
+        gui.style.width = "0%"
+    } else if (pass_length < 3) {
+        gui.style.width = "20%"
+    } else if (pass_length < 5) {
+        gui.style.width = "50%" 
+    } else if (pass_length < 7) {
+        gui.style.width = "80%"
+    } else if (pass_length < 8) {
+        gui.style.width = "100%"
+    }
+
+
+    if (pass_length === 0) {
+        showPower.textContent = "none"
+        return "none"
+    }
+    if (pass_length >= 5 && upper && lower && other && number) {
             if (pass_length >= 8){
                 showPower.textContent = "strong"
+                gui.style.backgroundColor = "green"
+                gui.style.maxWidth = "100%"
                 return "strong"
             }
         showPower.textContent = "medium"
+        gui.style.backgroundColor = "orange"
+        gui.style.maxWidth = "70%"
         return "medium"
     }
     showPower.textContent = "weak"
+    gui.style.backgroundColor = "red"
+    gui.style.maxWidth = "30%"
     return "weak"
 }
 
